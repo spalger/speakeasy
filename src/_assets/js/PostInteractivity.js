dbc8211be61e7a96b05cb7ecaacf42197c1739a8
spalger <spalger@users.noreply.github.com>
2018-04-25 15:55:45 -0700

    remove dokku and js stuff

--- src/_assets/js/PostInteractivity.js
+++
@@ -1,5 +0,0 @@
-var React = load('react/react');
-var CommentSection = load('CommentSection');
-
-var section = document.getElementById('posts-comments');
-React.render(React.createElement(CommentSection), section);
